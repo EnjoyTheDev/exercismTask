d848c5e82977e4cf933f0850e6a30f4d3fd708b8
Vladislav Seredinskiy <Vladislav08011993@outlook.com>
2020-04-01 08:42:23 +0300

    Exercism Task 1,2

---
+++ csharp/hello-world/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public static class HelloWorld
+{
+    public static string Hello()
+    {
+        string greeting = "Hello, World!";
+
+        return greeting;
+        
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
