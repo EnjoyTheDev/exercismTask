73635494f749d5e72e827515d572a3ed02256632
Vladislav Seredinskiy <Vladislav08011993@outlook.com>
2020-04-01 14:00:42 +0300

    TwoFer done

--- csharp/two-fer/TwoFerTests.cs
+++ csharp/two-fer/TwoFerTests.cs
@@ -4,21 +4,21 @@ using Xunit;
 
 public class TwoFerTests
 {
-    [Fact]
-    public void No_name_given()
-    {
-        Assert.Equal("One for you, one for me.", TwoFer.Speak());
-    }
+  [Fact]
+  public void No_name_given()
+  {
+    Assert.Equal("One for you, one for me.", TwoFer.Speak());
+  }
 
-    [Fact(Skip = "Remove to run test")]
-    public void A_name_given()
-    {
-        Assert.Equal("One for Alice, one for me.", TwoFer.Speak("Alice"));
-    }
+  [Fact]
+  public void A_name_given()
+  {
+    Assert.Equal("One for Alice, one for me.", TwoFer.Speak("Alice"));
+  }
 
-    [Fact(Skip = "Remove to run test")]
-    public void Another_name_given()
-    {
-        Assert.Equal("One for Bob, one for me.", TwoFer.Speak("Bob"));
-    }
+  [Fact]
+  public void Another_name_given()
+  {
+    Assert.Equal("One for Bob, one for me.", TwoFer.Speak("Bob"));
+  }
 }
