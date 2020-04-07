6e37debfc55b350a2de0b8f1240204797aed3f59
Vladislav Seredinskiy <Vladislav08011993@outlook.com>
2020-04-07 12:11:25 +0300

    added Exercism task

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        List<char> results = new List<char>();
+
+        char[] characters = word.ToLower().ToCharArray();
+
+        foreach (char character in characters)
+        {
+            if (char.IsWhiteSpace(character) || character.Equals('-'))
+            {
+                continue;
+            }
+
+            if (results.Contains(character))
+            {
+                return false;
+            }
+            else
+            {
+                results.Add(character);
+            }
+        }
+        return true;
+    }
+}
---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        bool[] targetMark = new bool[26];
+
+        var indexTargetMark = 0;
+
+        input = input.ToLower();
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            if ('a' <= input[i] && input[i] <= 'z')
+            {
+                indexTargetMark = input[i] - 'a';
+            }
+            targetMark[indexTargetMark] = true;
+        }
+
+        for (int i = 0; i <= 25; i++)
+        {
+            if (targetMark[i] == false)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+        //return input.ToLower().Where(input => char.IsLetter(input)).GroupBy(input => input).Count() == 26; // using Linq
+    }
+}
